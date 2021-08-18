3d2e45fe9fdbe0c068abdbe63f573c3c1e55111d
LukeTraynor <19008512@woodhouse.ac.uk>
2021-08-18 18:52:15 -0400

    code for json file implentation

--- Shared/People.swift
+++ Shared/People.swift
@@ -1,20 +1,28 @@
-//
-//  People.swift
-//  People
-//
-//  Created by Luke Traynor on 18/08/2021.
-//
-
+import Foundation
 import SwiftUI
+import CoreLocation
 
-struct People: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+struct People: Hashable, Codable {
+    var id: Int
+    var name: String
+    var place: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+        var image: Image {
+            Image(imageName)
+        }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude)
+        }
 
-struct People_Previews: PreviewProvider {
-    static var previews: some View {
-        People()
-    }
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
 }
