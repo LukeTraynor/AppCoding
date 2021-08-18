d588b6045e4432b955675947c3605531e2a73103
LukeTraynor <19008512@woodhouse.ac.uk>
2021-08-17 22:24:49 -0400

    Initial Commit

---
+++ Shared/VibratingIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibratingIOSApp.swift
+//  Shared
+//
+//  Created by Luke Traynor on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct VibratingIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
